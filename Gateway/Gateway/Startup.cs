84de3c39d34c86594a4fa6f610d7da954fac2a41
Volcov Oleg <bojikch15@gmail.com>
2022-09-13 21:13:17 +0300

    ASP.NET Core project initialization.

---
+++ Gateway/Gateway/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(Handler);
+        }
+        
+        private async Task Handler(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello!");
+        }
+    }
+}
