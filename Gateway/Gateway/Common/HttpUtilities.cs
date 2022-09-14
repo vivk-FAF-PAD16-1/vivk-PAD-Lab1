81cabb17b8b86661b46bd855ad4b73749d999731
Volcov Oleg <bojikch15@gmail.com>
2022-09-14 09:02:53 +0300

    Create http utilities to read body from request.

---
+++ Gateway/Gateway/Common/HttpUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Net;
+
+namespace Gateway.Common
+{
+    public static class HttpUtilities
+    {
+        public static string ReadRequestBody(HttpListenerRequest request)
+        {
+            var body = request.InputStream;
+            var encoding = request.ContentEncoding;
+
+            var reader = new StreamReader(body, encoding);
+            var content = reader.ReadToEnd();
+            
+            reader.Close();
+            body.Close();
+
+            return content;
+        }
+    }
+}
