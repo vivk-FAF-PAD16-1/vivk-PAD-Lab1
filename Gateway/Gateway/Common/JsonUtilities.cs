401846d989ec72ed1134bc5be59f0963f9c24cdf
Volcov Oleg <bojikch15@gmail.com>
2022-09-14 09:04:41 +0300

    Create json utilities to validate is string a json format.

---
+++ Gateway/Gateway/Common/JsonUtilities.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+
+namespace Gateway.Common
+{
+    public static class JsonUtilities
+    {
+        public static bool IsValid(string source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                JsonDocument.Parse(source);
+                return true;
+            }
+            catch (JsonException)
+            {
+                return false;
+            }
+        }
+    }
+}
