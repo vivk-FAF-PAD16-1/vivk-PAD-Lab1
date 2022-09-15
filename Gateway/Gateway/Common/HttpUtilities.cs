021369ee67101281ce396250a444130c152be810
Volcov Oleg <bojikch15@gmail.com>
2022-09-15 22:16:25 +0300

    Add new string extension methods in http utilities.

--- Gateway/Gateway/Common/HttpUtilities.cs
+++ Gateway/Gateway/Common/HttpUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace Gateway.Common
@@ -17,6 +18,41 @@ namespace Gateway.Common
             body.Close();
 
             return content;
+        }
+
+        private static readonly char[] WebCharacters = { '/', '\\' };
+
+        public static string TrimWeb(this string source)
+        {
+            return source.Trim(WebCharacters);
+        }
+
+        public static bool StartWith(this string source, string find)
+        {
+            var length = find.Length;
+            if (length > source.Length)
+            {
+                return false;
+            }
+            
+            for (var i = 0; i < length; i++)
+            {
+                if (find[i] != source[i])
+                {
+                    return false;
+                }
+            }
+
+            if (length != source.Length)
+            {
+                var webCharacter = source[length];
+                if (WebCharacters.Contains(webCharacter) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
