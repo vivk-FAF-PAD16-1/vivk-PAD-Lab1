4bb13512abe39d274922c444914ec15547ca6646
Volcov Oleg <bojikch15@gmail.com>
2022-09-15 22:51:48 +0300

    Create new gateway router to re-send data from request to service.

---
+++ Gateway/Gateway/Router/GatewayRouter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using Gateway.Common;
+using Gateway.Storage;
+
+namespace Gateway.Router
+{
+	public class GatewayRouter : IRouter
+	{
+		private IStorage _storage;
+        
+        private const string NotFoundMessage = "OLEG NOT FOUND!";
+        private const int NotFoundStatus = 404;
+
+        public GatewayRouter(IStorage storage)
+        {
+            _storage = storage;
+        }
+		public void Route(HttpListenerRequest request, HttpListenerResponse response)
+		{
+			var endpoint = request.Url.AbsolutePath.TrimWeb();
+			var (ok, uri) = _storage.TryGet(endpoint);
+			if (ok == false)
+			{
+				// TODO: NOT FOUND
+				return;
+			}
+			
+			Console.WriteLine(uri);
+		}
+	}
+}
