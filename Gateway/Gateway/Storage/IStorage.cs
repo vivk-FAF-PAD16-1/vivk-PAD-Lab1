b8a012665bf882674656632a3ad2e40261380df9
Volcov Oleg <bojikch15@gmail.com>
2022-09-15 13:03:17 +0300

    Create new interface for storage api.

---
+++ Gateway/Gateway/Storage/IStorage.cs
@@ -0,0 +1,10 @@
+namespace Gateway.Storage
+{
+    public interface IStorage
+    {
+        void Register(string endpoint, string destinationUri);
+        void Unregister(string endpoint, string destinationUri);
+
+        (bool, string) TryGet(string endpoint);
+    }
+}
