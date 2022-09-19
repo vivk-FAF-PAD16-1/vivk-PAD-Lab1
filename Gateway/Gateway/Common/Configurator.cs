bafe87ee746d33ca3f1225e89db5707735a4c4b6
Volcov Oleg <bojikch15@gmail.com>
2022-09-19 21:12:23 +0300

    Create new configurator file to load JSON data.

---
+++ Gateway/Gateway/Common/Configurator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Gateway.Common.Data;
+
+namespace Gateway.Common
+{
+    public class Configurator
+    {
+        private readonly string _path;
+        
+        public Configurator(string path)
+        {
+            _path = path;
+        }
+
+        public ConfigurationData Load()
+        {
+            if (File.Exists(_path) == false)
+            {
+                throw new Exception("Can not load configuration file.");
+            }
+
+            var jsonContent = File.ReadAllText(_path);
+            if (JsonUtilities.IsValid(jsonContent) == false)
+            {
+                throw new Exception("Configuration file is not in JSON  format.");
+            }
+            
+            var configurationData = JsonSerializer.Deserialize<ConfigurationData>(jsonContent,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return configurationData;
+        }
+    }
+}
