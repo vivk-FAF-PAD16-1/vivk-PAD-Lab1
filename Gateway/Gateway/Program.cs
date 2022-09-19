09832a462761f1a750b289bcb1e48b67cd879ea8
Volcov Oleg <bojikch15@gmail.com>
2022-09-19 20:28:45 +0300

    Modify main method to start gateway listener.

--- Gateway/Gateway/Program.cs
+++ Gateway/Gateway/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Gateway
+﻿using System.Threading;
+using Gateway.Listener;
+using Gateway.Router;
+
+namespace Gateway
 {
 	internal static class Program
 	{
 		public static void Main(string[] args)
 		{
+			const string discoveryUri = "http://localhost:40405/";
+			
+			var gatewayRouter = new GatewayRouter(discoveryUri);
+			
+			var prefixes0 = new[] { "http://localhost:40300/" };
+			var gatewayListener = new AsyncListener(prefixes0, gatewayRouter) as IAsyncListener;
+			gatewayListener.Schedule();
+
+			Thread.Sleep(Timeout.InfiniteTimeSpan);
 		}
 	}
 }
