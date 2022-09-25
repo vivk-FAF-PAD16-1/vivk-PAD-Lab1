2f69b004ce272f7f7393c7b178853fab95ade7c1
Volcov Oleg <bojikch15@gmail.com>
2022-09-25 21:29:36 +0300

    Create news router and configure endpoints.

---
+++ News/News/Router/NewsRouter.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using News.Common;
+using News.Endpoints;
+
+namespace News.Router
+{
+    public class NewsRouter : IRouter
+    {
+        private NewsEndpoints _endpoints;
+
+        private enum Router
+        {
+            Default = 1,
+            Id = 2,
+        }
+        
+        public NewsRouter()
+        {
+            _endpoints = new NewsEndpoints();
+        }
+        
+        public void Route(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            var segments = request.Url.Segments;
+            var segmentsCount = segments.Length;
+
+            const int defaultIndex = (int)Router.Default;
+            const int idIndex = (int)Router.Id;
+            
+            switch (segmentsCount)
+            {
+                case defaultIndex:
+                    _endpoints.RouteAll(request, response);
+                    break;
+                case idIndex:
+                    var id = segments[idIndex - 1].TrimWeb();
+                    _endpoints.RouteById(request, response, id);
+                    break;
+                default:
+                    HttpUtilities.NotFoundResponse(response);
+                    break;
+            }
+            
+            response.Close();
+        }
+    }
+}
