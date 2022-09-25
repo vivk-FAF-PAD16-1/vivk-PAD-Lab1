477744e44926457bcf7ee5feaf9a25854c86dd8b
Volcov Oleg <bojikch15@gmail.com>
2022-09-25 21:30:02 +0300

    Update main class for running news feed listener.

--- News/News/Program.cs
+++ News/News/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace News
+﻿using System;
+using System.IO;
+using System.Threading;
+using News.Common;
+using News.Listener;
+using News.Router;
+
+namespace News
 {
 	internal static class Program
 	{
+		private const string ConfigurationPath = "../../Resources/configuration.json";		
+		
 		public static void Main(string[] args)
 		{
+			var directoryAbsolutePath = AppDomain.CurrentDomain.BaseDirectory;
+			var configurationAbsolutePath = Path.Combine(
+				directoryAbsolutePath, ConfigurationPath);
+			
+			var configurator = new Configurator(configurationAbsolutePath);
+			var configurationData = configurator.Load();
+
+			var newsRouter = new NewsRouter() as IRouter;
+
+			var newsListener = new AsyncListener(
+				configurationData.NewsPrefixes,
+				newsRouter);
+			newsListener.Schedule();
+
+			Thread.Sleep(Timeout.InfiniteTimeSpan);
 		}
 	}
 }
