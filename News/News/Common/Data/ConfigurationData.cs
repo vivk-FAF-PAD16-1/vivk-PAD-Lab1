db27079f1a8f1cae329863b3d1d6cfe10e6913f2
Volcov Oleg <bojikch15@gmail.com>
2022-09-27 20:53:51 +0300

    Update configuration data by routes for service discovery.

--- News/News/Common/Data/ConfigurationData.cs
+++ News/News/Common/Data/ConfigurationData.cs
@@ -4,6 +4,7 @@
     {
         public string DiscoveryUri { get; set; }
         public string[] NewsPrefixes { get; set; }
+        public RouteData[] Routes { get; set; }
         
         public int MaxCount { get; set; }
     }
