316e38d93a510a0d0da09387f2dd17b636b343e1
Volcov Oleg <bojikch15@gmail.com>
2022-09-28 20:41:09 +0300

    Update news endpoint by news model to create new item.

--- News/News/Endpoints/NewsEndpoints.cs
+++ News/News/Endpoints/NewsEndpoints.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
 using News.Common;
+using News.Common.Data;
+using News.Model;
 
 namespace News.Endpoints
 {
@@ -9,12 +13,14 @@ namespace News.Endpoints
         private const string Post = "POST";
         private const string Put = "PUT";
 
+        private readonly NewsModel _model;
+
         public NewsEndpoints()
         {
-            
+            _model = new NewsModel("localhost", 3306, "root", "root");
         }
         
-        public void RouteAll(HttpListenerRequest request, HttpListenerResponse response)
+        public async Task RouteAll(HttpListenerRequest request, HttpListenerResponse response)
         {
             var method = request.HttpMethod;
 
@@ -24,7 +30,18 @@ namespace News.Endpoints
                     // TODO: get all news
                     break;
                 case Post:
-                    // TODO: create new news feed
+                    var body = HttpUtilities.ReadRequestBody(request);
+                    var isValid = JsonUtilities.IsValid(body);
+                    if (!isValid)
+                    {
+                        HttpUtilities.BadRequestResponse(response);
+                        return;
+                    }
+
+                    var data = JsonSerializer.Deserialize<NewsData>(body,
+                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    await _model.Create(data);
                     break;
                 default:
                     HttpUtilities.NotFoundResponse(response);
