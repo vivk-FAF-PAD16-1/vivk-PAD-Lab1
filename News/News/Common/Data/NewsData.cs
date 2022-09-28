8c6709b72d0c28c7238164841e14e2be2a630bee
Volcov Oleg <bojikch15@gmail.com>
2022-09-28 14:40:35 +0300

    Create new data for news item.

---
+++ News/News/Common/Data/NewsData.cs
@@ -0,0 +1,10 @@
+﻿namespace News.Common.Data
+{
+	public struct NewsData
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Content { get; set; }
+	}
+}
