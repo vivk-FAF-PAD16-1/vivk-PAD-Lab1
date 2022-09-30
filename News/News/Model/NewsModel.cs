7d5a642dceea20c1f76c4670bf02d8252e6e7bbc
Volcov Oleg <bojikch15@gmail.com>
2022-09-30 09:57:45 +0300

    Create model method to get item from db by id.

--- News/News/Model/NewsModel.cs
+++ News/News/Model/NewsModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using MySqlConnector;
+using News.Common;
 using News.Common.Data;
 
 namespace News.Model
@@ -10,9 +11,9 @@ namespace News.Model
 	public class NewsModel
 	{
 		private const string Database = "NewsSchema";
-		
+
 		private MySqlConnection _connection;
-		
+
 		public NewsModel(string server, int port, string userId, string password)
 		{
 			var builder = new MySqlConnectionStringBuilder
@@ -29,32 +30,32 @@ namespace News.Model
 		public async Task Create(NewsData data)
 		{
 			await _connection.OpenAsync();
-			
+
 			var command = _connection.CreateCommand();
-			
+
 			command.CommandText = "INSERT INTO News(Title,Description,Content,Date)" +
 			                      "VALUES(@title,@desc,@content,@date)";
 			command.Parameters.AddWithValue("@title", data.Title);
 			command.Parameters.AddWithValue("@desc", data.Description);
 			command.Parameters.AddWithValue("@content", data.Content);
 			command.Parameters.AddWithValue("@date", DateTime.Now);
-			
+
 			await command.ExecuteNonQueryAsync();
-			
+
 			await _connection.CloseAsync();
 		}
 
 		public async Task Get(int number, List<NewsData> dest)
 		{
 			await _connection.OpenAsync();
-			
+
 			var command = _connection.CreateCommand();
 
 			command.CommandText = "SELECT * FROM News " +
 			                      "ORDER BY Date DESC " +
 			                      "LIMIT @number";
 			command.Parameters.AddWithValue("@number", number);
-			
+
 			var reader = await command.ExecuteReaderAsync();
 			while (reader.Read())
 			{
@@ -65,11 +66,41 @@ namespace News.Model
 					Description = (string)reader["Description"],
 					Content = (string)reader["Content"],
 				};
-				
+
 				dest.Add(data);
+			}
+
+			await _connection.CloseAsync();
+		}
+
+		public async Task<(NewsData, bool)> Get(int id)
+		{
+			await _connection.OpenAsync();
+
+			var command = _connection.CreateCommand();
+
+			command.CommandText = "SELECT * FROM News " +
+			                      "WHERE Id=@id";
+			command.Parameters.AddWithValue("@id", id);
+
+			var reader = await command.ExecuteReaderAsync();
+			
+			var data = new NewsData();
+			var isFound = false;
+			while (reader.Read())
+			{
+				data.Id = (int)reader["Id"];
+				data.Title = (string)reader["Title"];
+				data.Description = (string)reader["Description"];
+				data.Content = (string)reader["Content"];
+
+				isFound = true;
+				break;
 			}
 			
 			await _connection.CloseAsync();	
+
+			return (data, isFound);
 		}
 	}
 }
