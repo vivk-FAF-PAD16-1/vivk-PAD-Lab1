77823f693db29410b1df57cb932b1d3eba4f375c
Volcov Oleg <bojikch15@gmail.com>
2022-10-05 21:56:17 +0300

    Create new model to sync with database auth data.

---
+++ News/News/Model/AuthModel.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+using MongoDB.Driver;
+using News.Common;
+
+namespace News.Model
+{
+	public class AuthModel
+	{
+		private readonly IMongoCollection<AuthData> _collection;
+
+		public AuthModel(string connection, string dbName, string collectionName)
+		{
+			var client = new MongoClient(connection);
+			var database = client.GetDatabase(dbName);
+			_collection = database.GetCollection<AuthData>(collectionName);
+			
+			/*
+			collection.InsertOne(new AuthData
+			{
+				Email = "name@email.mail",
+				Pass = "Kek",
+			});
+			*/
+		}
+
+		public async Task<bool> Create(AuthData data)
+		{
+			var list = await _collection.Find(x => x.Email == data.Email)
+				.ToListAsync();
+
+			if (list != null)
+			{
+				if (list.Count != 0)
+				{
+					return false;
+				}
+			}
+
+			await _collection.InsertOneAsync(data);
+
+			return true;
+		}
+		
+		public async Task<bool> Get(AuthData data)
+		{
+			var list = await _collection.Find(x => x.Email == data.Email && 
+			                                       x.Pass == data.Pass)
+				.ToListAsync();
+
+			if (list == null)
+			{
+				return false;
+			}
+
+			if (list.Count == 0)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
